f4e88d2b5f1df694392134f4bfc94a3ae571c1fe
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-10 19:51:51 -0600

    with challenge

--- src/index.js
+++ src/index.js
@@ -1,41 +1,51 @@
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
-let testNumbers = numbers.map((x) => {
-  return x > 10;
+// //Map -Create a new array by doing something with each item in an array.
+// // let testNumbers = numbers.map((x) => {
+// //   return x > 10;
+// // });
+
+// // console.log("Map: " + testNumbers);
+
+// //Filter - Create a new array by keeping the items that return true.
+// let anotherTest = numbers.filter((x) => {
+//   return x == 56;
+// });
+
+// console.log("Filter: " + anotherTest);
+
+// //Reduce - Accumulate a value by doing something to each item in an array.
+// let reduceTest = numbers.reduce((accumulator, currentValue) => {
+//   console.log("CurrentValue: " + currentValue);
+//   console.log("Accumulator: " + accumulator);
+
+//   return accumulator + currentValue;
+// });
+
+// console.log(reduceTest);
+
+// //Find - find the first item that matches from an array.
+
+// let findTest = numbers.find((x) => {
+//   return x > 7;
+// });
+
+// console.log(findTest);
+
+// //FindIndex - find the index of the first item that matches.
+
+// let findIndexTest = numbers.findIndex((x) => {
+//   return x == 5;
+// });
+
+// console.log(findIndexTest);
+
+import emojipedia from "./emojipedia";
+
+let result = emojipedia.map((entry) => {
+  return entry.meaning;
 });
 
-console.log("Map: " + testNumbers);
-
-//Filter - Create a new array by keeping the items that return true.
-let anotherTest = numbers.filter((x) => {
-  return x > 10;
+result.map((x) => {
+  console.log(x.substring(0, 100));
 });
-
-console.log("Filter: " + anotherTest);
-
-//Reduce - Accumulate a value by doing something to each item in an array.
-let reduceTest = numbers.reduce((accumulator, currentValue) => {
-  console.log("CurrentValue: " + currentValue);
-  console.log("Accumulator: " + accumulator);
-
-  return accumulator + currentValue;
-});
-
-console.log(reduceTest);
-
-//Find - find the first item that matches from an array.
-
-let findTest = numbers.find((x) => {
-  return x > 7;
-});
-
-console.log(findTest);
-
-//FindIndex - find the index of the first item that matches.
-
-let findIndexTest = numbers.findIndex((x) => {
-  return x == 5;
-});
-
-console.log(findIndexTest);
